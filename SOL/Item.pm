87b6a50bdb68154ec5b463822275a8077fc21dda
Lars Stoltenow <penma@penma.de>
2010-12-04 16:13:49 +0100

    rewrite SOL::Item
    unknown if there will be other objects like SOL::Coin.
    
    Much more important are the other objects (fgrep -L ::C:: SOL/*.pm)

--- SOL/Item.pm
+++ SOL/Item.pm
@@ -2,42 +2,35 @@ package SOL::Item;
 
 use strict;
 use warnings;
-use 5.010;
 
-use List::MoreUtils qw(first_index);
-use Readonly;
+use Class::XSAccessor {
+	accessors => {
+		position => "position",
+		type => "type",
+		value => "value",
+	},
+	constructor => "new",
+};
 
-use SOL::Coordinates;
+use SOL::C::Item;
+use SOL::Util::Coordinates;
 
-Readonly my @item_types => (qw(none coin grow shrink));
-
-sub new {
-	my ($class, %args) = @_;
-	bless({
-		position => $args{position},
-		type     => $args{type},
-		value    => $args{value},
-	}, $class);
-}
-
-sub from_sol {
-	my ($class, $sol) = @_;
-
-	my @p = $sol->get_float(3);
-	my ($t, $v) = $sol->get_index(2);
-
+sub from_c {
+	my ($class, $file, $cobj) = @_;
 	$class->new(
-		position => [ SOL::Coordinates::neverball_to_radiant(@p) ],
-		type     => $item_types[$t],
-		value    => $v,
+		position => [ SOL::Util::Coordinates::neverball_to_radiant($cobj->position) ],
+		type     => $cobj->type,
+		value    => $cobj->value,
 	);
 }
 
-sub to_sol {
-	my ($self, $sol) = @_;
-
-	$sol->put_float(SOL::Coordinates::radiant_to_neverball(@{$self->{position}}));
-	$sol->put_index(first_index(sub { $_ eq $self->{type} }, @item_types), $self->{value});
+sub to_c {
+	my ($self, $file) = @_;
+	$file->store_object("item", SOL::C::Item->new(
+		position => [ SOL::Util::Coordinates::radiant_to_neverball(@{$self->{position}}) ],
+		type     => $self->{type},
+		value    => $self->{value},
+	));
 }
 
 1;
@@ -46,9 +39,18 @@ __END__
 
 =head1 NAME
 
-SOL::Item - s_item
+SOL::C::Side - s_side
 
 =head1 SYNOPSIS
 
+ my $s = SOL::C::Side->from_sol($reader);
+ my @n = $s->normal;
+ my $d = $s->distance;
+
+ my $s = SOL::C::Side->new(normal => [ 0.70, 0.70, 0 ], distance => -5);
+ $s->to_sol($writer);
+
 =head1 DESCRIPTION
 
+A SOL::C::Side is the exact representation of an s_side structure. The
+coordinates of the normal vector are in the Neverball coordinate system.
