602318b29091542ff0650e2bf138b03bab996049
Lars Stoltenow <penma@penma.de>
2010-12-12 17:47:07 +0100

    SOL::Node: refactoring

--- SOL/Node.pm
+++ SOL/Node.pm
@@ -16,27 +16,30 @@ sub new {
 sub to_c {
 	my ($self, $file) = @_;
 
-	# store lumps.
-	# note that the lumps have to be stored sequentially in the file.
-	# unlike all other objects, lumps are not resolved through the index
-	# vector.
-	# so we just assume that they are stored sequentially, and, to be
-	# extra safe, validate that this is indeed the case.
-	# at least mapc doesn't seem to ensure this either.
-	my @li = map $_->to_c($file), @{$self->{lumps}};
+	my %params;
 
-	# die if for some reason the lump indices are not adjacent
-	for my $x (1 .. @li - 1) {
-		if ($li[$x] != $li[$x - 1] + 1) {
-			die("Indices of lumps are not adjacent. This should not happen.");
+	if (@{$self->{lumps}}) {
+		# store lumps.
+		# note that the lumps have to be stored sequentially in the file.
+		# unlike all other objects, lumps are not resolved through the index
+		# vector.
+		# so we just assume that they are stored sequentially, and, to be
+		# extra safe, validate that this is indeed the case.
+		# at least mapc doesn't seem to ensure this either.
+		my @li = map $_->to_c($file), @{$self->{lumps}};
+
+		# die if for some reason the lump indices are not adjacent
+		for my $x (1 .. @li - 1) {
+			if ($li[$x] != $li[$x - 1] + 1) {
+				die("Indices of lumps are not adjacent. This should not happen.");
+			}
 		}
+
+		$params{lump_first} = $li[0];
+		$params{lump_count} = scalar(@li);
 	}
 
-	$file->store_object("node", SOL::C::Node->new(
-		# XX
-		lump_first => $li[0],
-		lump_count => scalar(@li),
-	));
+	$file->store_object("node", SOL::C::Node->new(%params));
 }
 
 1;
